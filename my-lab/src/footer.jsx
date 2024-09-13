af6779c2cf32b09bc616bf33046b1b05d1c48f30
Rubick1337 <poss1337@gmail.com.>
2024-09-13 15:05:15 +0300

    feat: add components

--- my-lab/src/footer.jsx
+++ my-lab/src/footer.jsx
@@ -8,7 +8,7 @@ import klama from './images/klama.png';
 import fsc from '../src/images/fsc.png';
 import oneTree from '../src/images/one-tree.png';
 import trustpilot from '../src/images/trustpilot.png';
-
+import LinkSection from './links-footer';
 const paymentImages = [
     { src: visa, alt: 'visa', width: 66, height: 28 },
     { src: paypal, alt: 'paypal', width: 66, height: 28 },
@@ -23,44 +23,7 @@ class Footer extends Component {
       <footer>
         <div className="container-footer">
           <div className="contatcs">
-            <div className="about-skills About-size">
-              <div className="title-contacts">
-                <h2 data-i18n="footer-about-title">About</h2>
-              </div>
-              <div className="text-contacts">
-                <a href="#" data-i18n="footer-about-us">About Us</a>
-                <a href="#" data-i18n="footer-design">Design</a>
-                <a href="#" data-i18n="footer-sustainability">Sustainability</a>
-                <a href="#" data-i18n="footer-plant-trees">We plant trees</a>
-                <a href="#" data-i18n="footer-our-story">Our Story</a>
-                <a href="#" data-i18n="footer-awards">Awards</a>
-              </div>
-            </div>
-            <div className="about-skills contacts-size">
-              <div className="title-contacts">
-                <h2 data-i18n="footer-help-title">Help</h2>
-              </div>
-              <div className="text-contacts">
-                <a href="#" data-i18n="footer-holiday-offers-faq">Holiday Offers FAQ</a>
-                <a href="#" data-i18n="footer-faq">FAQ</a>
-                <a href="#" data-i18n="footer-warranty">Warranty</a>
-                <a href="#" data-i18n="footer-shipments">Shipments</a>
-                <a href="#" data-i18n="footer-returns-claims">Returns & Claims</a>
-                <a href="#" data-i18n="footer-contact-us">Contact us</a>
-              </div>
-            </div>
-            <div className="about-skills More-size">
-              <div className="title-contacts">
-                <h2 data-i18n="footer-more-title">More</h2>
-              </div>
-              <div className="text-contacts">
-                <a href="#" data-i18n="footer-where-to-find-us">Where to find us</a>
-                <a href="#" data-i18n="footer-family">Woodendot Family</a>
-                <a href="#" data-i18n="footer-press">Press</a>
-                <a href="#" data-i18n="footer-affiliates">Affiliates</a>
-                <a href="#" data-i18n="footer-video-lifestyle">Video Lifestyle</a>
-              </div>
-            </div>
+            <LinkSection/>
             <div className="raiting-company">
               <div className="text-raiting">
                 <h3 data-i18n="footer-news-title">Don’t miss any news and exclusive offers!</h3>
--- my-lab/src/header.jsx
+++ my-lab/src/header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import image from './images/IMAGE.png';
-
+import Navigation from './navigation'
 class Header extends Component {
   render() {
     return (
@@ -8,53 +8,7 @@ class Header extends Component {
         <div className="header-site">
           <div className="container-navigation">
             <div className="navigation">
-              <nav>
-                <ul className="menu-navigation">
-                  <li>
-                    <a href="#Wooden-Furniture" data-i18n="menu-wooden-furniture">
-                      Wooden Furniture
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#About" data-i18n="menu-about">About</a>
-                  </li>
-                  <li>
-                    <a href="#Stories" data-i18n="menu-stories">Stories</a>
-                  </li>
-                  <li>
-                    <a href="admin-panel.html" data-i18n="Admin" id="admin">Admin</a>
-                  </li>
-                  <div className="buttons">
-                    <label className="switch">
-                      <input type="checkbox" className="switch-input" />
-                      <span className="switch-slider"></span>
-                    </label>
-                    <div className="moon"></div>
-                    <div className="dropdown">
-                      <button className="dropbtn" type="button">RU</button>
-                      <div className="dropdown-content">
-                        <button className="language-btn" type="button" id="button-ru">
-                          RU
-                        </button>
-                        <button className="language-btn" type="button" id="button-en">
-                          EN
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="img-profile background-settings"></div>
-                  <div id="dropdownMenu" className="dropdown-menu">
-                    <div className="login-register-menu-header">
-                      <a href="../login.html">Login</a>
-                      <a href="../registration.html">Register</a>
-                    </div>
-                    <div className="user-menu-header">
-                      <span>Welcome, <span className="username-display-header"></span>!</span>
-                      <button id="logoutButton-header">Log out</button>
-                    </div>
-                  </div>
-                </ul>
-              </nav>
+             <Navigation/>
             </div>
             <div className="burger-menu">
               <span className="line-burger"></span>
